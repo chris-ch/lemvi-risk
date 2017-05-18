4bbdb8b99906175821e7c22411dfdeb7a9d7c7a1
Christophe <ch.alexandre@bluewin.ch>
2017-05-18 16:27:17 +0200

    dev

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='lemvi-risk',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='christophe',
+    author_email='christophe.alexandre@lemvi.ch',
+    description='', requires=['slackclient']
+)
